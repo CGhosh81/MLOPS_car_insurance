02bcf72628a367c976baf5e57afcea16a65b562a
CGhosh81 <chayan07ghosh@gmail.com>
2025-07-25 13:10:02 +0530

    just start the project

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+  name="src",
+  version="0.0.1",
+  author="Chayan Ghosh",
+  author_email="chayan07ghosh@gmail.com",
+  packages=find_packages()
+ )
